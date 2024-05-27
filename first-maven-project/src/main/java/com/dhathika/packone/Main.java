e069f186c0ba0e9efd20f2a0566bbb7e7230cf64
dhathika <dhathikasuresh@gmail.com>
2024-05-27 06:34:53 +0530

    committing changes for main class

--- first-maven-project/src/main/java/com/dhathika/packone/Main.java
+++ first-maven-project/src/main/java/com/dhathika/packone/Main.java
@@ -3,7 +3,7 @@ package com.dhathika.packone;
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("this class and its package are new to git ");
+	System.out.println("this class and ");
 
 	}
 
